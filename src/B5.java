2e59f83d1a5e363e035c221a93d77b2eb544b4e0
H798979 <798979@stud.hvl.no>
2024-10-07 12:49:38 +0200

    Update B5.java

--- src/B5.java
+++ src/B5.java
@@ -2,12 +2,14 @@ import javax.swing.JOptionPane;
 
 public class B5 {
 	public static void main(String[] args) {
-		String PoengsumStr = JOptionPane.showInputDialog("Poengsum");
+		for (int i = 0; i < 10; i++) {
+		String PoengsumStr = JOptionPane.showInputDialog("Poengsum" + (i + 1));
 		int Poengsum = Integer.parseInt (PoengsumStr);
 		String karakter;
 		
 		
-		if (Poengsum < 0 || Poengsum > 100) {
+	
+		 if (Poengsum < 0 || Poengsum > 100) {
 			karakter = "Ugyldig peongsum";
 		} else if (Poengsum >= 90) {
 			karakter = "A";
@@ -27,7 +29,8 @@ public class B5 {
 		} else  {
 			karakter = "F";
 	}
-		JOptionPane.showMessageDialog(null, "Karakter: " + karakter);
+		JOptionPane.showMessageDialog(null, "Karakter: " + (i + 1) + " - Karakter: "+ karakter);
   
-}
+ }
+	}
 }
